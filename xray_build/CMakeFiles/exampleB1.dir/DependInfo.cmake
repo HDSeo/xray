68a286199a98de6c8f2d17cf2c1aaab32e88acc2
mac_ubuntu <centrifugal4@khu.ac.kr>
2014-11-07 12:53:01 +0900

    Add default xray & xray_build directory

---
+++ xray_build/CMakeFiles/exampleB1.dir/DependInfo.cmake
@@ -0,0 +1,51 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/seohyeondeog/Git/xray/xray/exampleB1.cc" "/home/seohyeondeog/Git/xray/xray_build/CMakeFiles/exampleB1.dir/exampleB1.cc.o"
+  "/home/seohyeondeog/Git/xray/xray/src/B1ActionInitialization.cc" "/home/seohyeondeog/Git/xray/xray_build/CMakeFiles/exampleB1.dir/src/B1ActionInitialization.cc.o"
+  "/home/seohyeondeog/Git/xray/xray/src/B1DetectorConstruction.cc" "/home/seohyeondeog/Git/xray/xray_build/CMakeFiles/exampleB1.dir/src/B1DetectorConstruction.cc.o"
+  "/home/seohyeondeog/Git/xray/xray/src/B1EventAction.cc" "/home/seohyeondeog/Git/xray/xray_build/CMakeFiles/exampleB1.dir/src/B1EventAction.cc.o"
+  "/home/seohyeondeog/Git/xray/xray/src/B1PrimaryGeneratorAction.cc" "/home/seohyeondeog/Git/xray/xray_build/CMakeFiles/exampleB1.dir/src/B1PrimaryGeneratorAction.cc.o"
+  "/home/seohyeondeog/Git/xray/xray/src/B1Run.cc" "/home/seohyeondeog/Git/xray/xray_build/CMakeFiles/exampleB1.dir/src/B1Run.cc.o"
+  "/home/seohyeondeog/Git/xray/xray/src/B1RunAction.cc" "/home/seohyeondeog/Git/xray/xray_build/CMakeFiles/exampleB1.dir/src/B1RunAction.cc.o"
+  "/home/seohyeondeog/Git/xray/xray/src/B1SteppingAction.cc" "/home/seohyeondeog/Git/xray/xray_build/CMakeFiles/exampleB1.dir/src/B1SteppingAction.cc.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "G4INTY_USE_QT"
+  "G4INTY_USE_XT"
+  "G4MULTITHREADED"
+  "G4UI_USE"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4VERBOSE"
+  "G4VIS_USE"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4VIS_USE_OPENGLX"
+  "G4VIS_USE_RAYTRACERX"
+  "G4_STORE_TRAJECTORY"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/seohyeondeog/geant4.10.00.p02/include/Geant4"
+  "/home/seohyeondeog/clhep/include"
+  "/usr/include/qt4"
+  "/usr/include/qt4/QtCore"
+  "/usr/include/qt4/QtGui"
+  "/usr/include/qt4/QtOpenGL"
+  "/home/seohyeondeog/Git/xray/xray/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
